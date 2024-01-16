35a88b20990cda13a03d81504de6442d13b2de12
Ashish Pandey <ashish.apandey@hotmail.com>
2024-01-15 23:43:45 -0600

    Testing download resume azure function

--- src/functions/httpTrigger-download-resume.js
+++ src/functions/httpTrigger-download-resume.js
@@ -1,13 +1,25 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+const { BlobServiceClient } = require("@azure/storage-blob");
 
-app.http('httpTrigger-download-resume', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+app.http("httpTrigger-download-resume", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    const blobServiceClient = await BlobServiceClient.fromConnectionString(
+      process.env.CONN_STRING
+    );
+    const containerClient = await blobServiceClient.getContainerClient(
+      process.env.CONTAINER
+    );
+    const blobClient = await containerClient.getBlobClient(process.env.BLOB);
+    const response = await blobClient.download(0);
 
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
+    return {
+      headers: {
+        "Content-Type": response.contentType,
+        "Content-Disposition": `attachment; filename="Ashish-Pandey-Resume.pdf"`,
+      },
+      body: response.readableStreamBody,
+    };
+  },
 });
